2eaee63a77f42a6491fbf8bb59d07ea03b61aeab
ATJezova <anna-triin.jezova@tktk.ee>
2023-05-09 22:06:53 +0300

    add week7 to code

---
+++ ArgumentsAndPerimeters/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Enter a word");
+string userImput = Console.ReadLine();
+
+PrintAnyWord(userImput); //userImput - argument
+
+static void PrintAnyWord(string anyString) //anyString - parameter
+{
+    anyString = anyString.ToUpper();
+    for(int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
---
+++ Week7/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Are ou coming or leaving? (in/out)");
+string userChoice = Console.ReadLine();
+
+if(userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodbye();
+}
+
+static void PrintHello()
+{
+    Console.WriteLine("hello, world!");
+}
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("see you later, aligator");
+}
